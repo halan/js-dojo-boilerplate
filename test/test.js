e89d87cc39e4f07bb3d10beecab5e624b93bc38b
Halan Pinheiro <halan.pinheiro@codeminer42.com>
2017-07-19 15:50:28 -0300

    Add some sinon examples

--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
 const expect = require('chai').expect;
+const sinon = require('sinon'); 
 
 describe('First test', () => {
   it('should test a basic thing; 1 equal 1', () => {
-    expect(1).to.equal(1);
+    expect(1).to.be.equal(1);
+  });
+
+
+  it("stub 42 as callback return", function () {
+    const callback = sinon.stub().returns(42);
+
+    expect(callback()).to.be.equal(42);
+  });
+
+
+  it("ensure thats callback was called", function () {
+    const callback = sinon.spy();
+
+    callback();
+
+    expect(callback.called).to.be.true;
   });
 });
